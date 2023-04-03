0c7094b984e7fb46218adb20dd91f296245de165
aryasugara <sugara232425@gmail.com>
2023-04-03 09:48:54 +0700

    Initial commit di commit yaaa

---
+++ app/src/main/java/com/example/tikjek/HalamanGojek.java
@@ -0,0 +1,14 @@
+package com.example.tikjek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalamanGojek extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_halaman_gojek);
+    }
+}
